7a9e7b76a391abf7f30cf9d3e613891ffc9d054f
VarshaPednekar2005 <varshapednekar136@gmail.com>
2026-02-27 06:39:29 +0000

    added dockerfile

--- START_SERVER.sh
+++ START_SERVER.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/varshapednekar/projects/gee-data-pipeline
 source venv/bin/activate
-uvicorn clean_downloader:app --host 127.0.0.1 --port 8000 --reload
 uvicorn app.main:app --reload
